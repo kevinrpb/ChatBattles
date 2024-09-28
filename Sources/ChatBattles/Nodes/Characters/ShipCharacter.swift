956c6fddcdba6cd9fab234b063c67d0e7b977d74
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2024-09-28 15:25:16 -0400

    Add ships

--- Sources/ChatBattles/Nodes/Characters/ShipCharacter.swift
+++ Sources/ChatBattles/Nodes/Characters/ShipCharacter.swift
@@ -2,6 +2,12 @@ import SwiftGodot
 
 @Godot
 public final class ShipCharacter: CharacterBody2D {
+	@SceneTree(path: "Sprite")
+	var sprite: Sprite2D?
+
+	var size: Vector2 {
+		sprite?.texture?.getSize() ?? .zero
+	}
 }
 
 extension ShipCharacter: LoadableScene {
--- Sources/ChatBattles/Nodes/Scenes/GameScene.swift
+++ Sources/ChatBattles/Nodes/Scenes/GameScene.swift
@@ -5,18 +5,48 @@ public final class GameScene: Node2D {
 	private var viewport: Viewport? { getViewport() }
 
 	public override func _ready() {
-		guard let viewport else { fatalError("No viewport??") }
+	}
+
+	public override func _unhandledInput(event: InputEvent?) {
+		guard
+			let viewport,
+			let event = event as? InputEventMouseButton,
+			event.pressed, !event.isEcho(), event.buttonIndex == .left
+		else {
+			return
+		}
 
 		addShip(in: viewport.getVisibleRect())
 	}
 
-	private func addShip(in rect: Rect2) {
+	private func addShips(_ n: Int, in rect: Rect2, animate: Bool = true) {
+		for _ in 0..<n { addShip(in: rect, animate: animate) }
+	}
+
+	private func addShip(in rect: Rect2, animate: Bool = true) {
+		let x = Float.random(in: 0...rect.size.x)
+		let y = Float.random(in: 0...rect.size.y)
+
+		addShip(at: Vector2(x: x, y: y), animate: animate)
+	}
+
+	private func addShip(at position: Vector2, animate: Bool = true) {
 		let ship = ShipCharacter.instantiate()
+
+		ship.position.x = position.x
+		ship.position.y = position.y
+
+		ship.modulate.alpha = animate ? 0 : 1
 
 		addChild(node: ship)
 
-		ship.position.x = rect.size.x / 2
-		ship.position.y = rect.size.y / 2
+		if animate {
+			let _ = ship.createTween()?
+				.tweenProperty(
+					object: ship, property: "modulate:a", finalVal: .init(1.0), duration: 1.0)?
+				.setTrans(.quad)?
+				.setEase(.in)
+		}
 	}
 }
 
